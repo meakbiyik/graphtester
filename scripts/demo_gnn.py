2eb43c7c63f711b8216c1126118c1ccf0d6015c4
M. Eren Akbiyik <erenakbiyik@gmail.com>
2023-01-24 22:42:49 +0100

    feat: use new feature_names functionality

--- scripts/demo_gnn.py
+++ scripts/demo_gnn.py
@@ -1,13 +1,11 @@
-from model import GNN
-from train import train
-
-from torch_geometric.nn import GINConv, GCNConv, GATv2Conv
-
+"""Experiments with GNNs on MUTAG dataset."""
 import shutil
-import torch
 from pathlib import Path
 
+from model import GNN
 from torch_geometric.datasets import TUDataset
+from torch_geometric.nn import GATv2Conv, GCNConv, GINConv  # noqa: F401
+from train import train
 
 import graphtester as gt
 
@@ -21,22 +19,17 @@ dataset = TUDataset(
     name="MUTAG",
     pre_transform=gt.pretransform(
         features=["Closeness centrality", "Eigenvector centrality"],
-        encode=True,
-        encode_together=True,
+        feature_names="x",
     ),
 )
 
 print(dataset[0].x)
-print(dataset[0]["_encoding"])
+print(dataset[0].x.shape)
+node_feature_count = dataset[0].x.shape[1]
+num_classes = dataset.num_classes
 
-
-model = GNN(8, 2, 20, 0.1, conv=GINConv)
+model = GNN(node_feature_count, num_classes, 20, 0.1, conv=GINConv)
 train(dataset, model)
-
-
-
-
-
 
 # remove the pretransformed dataset from the folder
 shutil.rmtree(SCRIPT_PATH / "data" / "MUTAG", ignore_errors=True)
