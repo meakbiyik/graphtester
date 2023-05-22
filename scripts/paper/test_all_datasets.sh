e3181deea7bf74c1da28de3b923b66f4ec221a54
M. Eren Akbiyik <erenakbiyik@gmail.com>
2023-05-22 18:43:41 +0200

    feat: do subsampling beffore converting graphs

--- scripts/paper/test_all_datasets.sh
+++ scripts/paper/test_all_datasets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=graphtester
 #SBATCH --mem=250G
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=10
 
 # Run the test_all_datasets.py script
-yes | poetry run python ./scripts/paper/test_all_datasets.py
-
+source $(poetry env info --path)/bin/activate
+yes | python ./scripts/paper/test_all_datasets.py
